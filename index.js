b7328b10d20bc2856e90d62ee2674ddba1e22549
Martin Villalba <14tinchov@gmail.com>
2019-06-10 19:22:20 -0300

    Update documentation and added scopes params to main function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var KJUR = require('jsrsasign');
 var axios = require('axios');
 
 var EasyGoogleToken = {
-  getTokenWithServiceAccountFile(creds = {} ) {
+  getTokenWithServiceAccountFile(creds = {}, scopes, callback ) {
     if (Object.keys(creds).length == 0) { return 'Need Service Account File to continue' };
     if (creds.private_key == null)      { return 'Need private_key in your File to continue' };
     if (creds.private_key_id == null)   { return 'Need private_key_id in your File to continue' };
@@ -21,7 +21,7 @@ var EasyGoogleToken = {
     const payload = {
       iss: creds.client_email,
       sub: creds.client_email,
-      scope: 'https://www.googleapis.com/auth/dialogflow',
+      scope: scopes,
       iat: KJUR.jws.IntDate.get('now'),
       exp: KJUR.jws.IntDate.get('now + 1hour'),
       aud: 'https://www.googleapis.com/oauth2/v4/token'
@@ -31,18 +31,17 @@ var EasyGoogleToken = {
     const stringPayload = JSON.stringify(payload);
     var token = KJUR.jws.JWS.sign('RS256', stringHeader, stringPayload, creds.private_key);
 
-
     axios.post('https://www.googleapis.com/oauth2/v4/token', {
         grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
         assertion: token
       })
       .then(function (response) {
         // handle success
-        console.log(response);
+        callback(response);
       })
       .catch(function (error) {
         // handle error
-        console.log(error);
+        callback(error);
       })
       .finally(function () {
         // always executed
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Easy Google Token
+
+Easy Google Token is a wasy way to get a token with a [Service Account File](https://cloud.google.com/iam/docs/creating-managing-service-account-keys])
+
+_*I recommend not using it directly in the Frontend, you could use it in the Backend to get a token and use it in the Frontend or you could create a file with limited access to use it in the Frontend_
+
+## Installing
+
+Using npm:
+
+```bash
+$ npm install easy_google_token
+```
+
+## Using
+
+```js
+# file: Json File with the service credentials
+# scopes: String (scopes separated by comma)
+# callback_function: function that will be executed when you receive the token
+
+EasyGoogleToken.getTokenWithServiceAccountFile(file, scopes , callback_function);
+```
+
+## Example
+
+```js
+const creds = require('./service_account_file.json');
+const scopes = 'https://www.googleapis.com/auth/dialogflow'
+EasyGoogleToken.getTokenWithServiceAccountFile(creds, scopes, (response) => { console.log(response.data.access_token); });
+```
